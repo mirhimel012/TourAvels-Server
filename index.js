0652cbc6afb6e4beb4d3f2f98079588c5443c889
himelmia535 <himelmia535@gmail.com>
2024-04-30 16:47:17 +0600

    get and post update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,7 +37,7 @@ async function run() {
         app.get('/touristsSpot/:id', async(req, res) => {
           const id = req.params.id;
           const query = {_id: new ObjectId(id)}
-          const result = await coffeeCollection.findOne(query);
+          const result = await touristsSpotCollection.findOne(query);
           res.send(result);
       })
 
