2028f1e09025b7bb76cd828a9c1c0bc518086c6a
himelmia535 <himelmia535@gmail.com>
2024-04-30 23:22:53 +0600

    done

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7).
-    await client.connect();
+
+    //
+    // await client.connect();
 
     const touristsSpotCollection = client.db('touristsSpotDB').collection('touristsSpot');
 
@@ -62,8 +64,6 @@ async function run() {
                 seasonality: updatedSpot.seasonality, 
                 travel_time: updatedSpot.travel_time, 
                 totaVisitorsPerYear: updatedSpot.totaVisitorsPerYear, 
-                username: updatedSpot.username,
-                useremail: updatedSpot.useremail,
                 message: updatedSpot.message,
                 location: updatedSpot.location,
                 photo: updatedSpot.photo
@@ -84,7 +84,10 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
+    //
+    // await client.db("admin").command({ ping: 1 });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
